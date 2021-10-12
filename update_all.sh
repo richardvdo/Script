f4a3119084e505491609e50aff2ed35440fd6257
richardvo <richardvo@gmail.com>
2021-10-12 10:27:36 +0200

    ajout pi4

--- update_all.sh
+++ update_all.sh
@@ -3,4 +3,5 @@ sshpass -p 66446644 sudo ssh pi@192.168.1.54 /home/pi/scripts/update.sh &
 sshpass -p 66446644 sudo ssh pi@192.168.1.55 /home/pi/scripts/update.sh &
 sshpass -p 66446644 sudo ssh pi@192.168.1.58 /home/pi/scripts/update.sh &
 sshpass -p 66446644 sudo ssh pi@192.168.1.92 /home/pi/scripts/update.sh &
-pihole
+sshpass -p 66446644 sudo ssh pi@192.168.1.232 /home/pi/scripts/update.sh &
+
