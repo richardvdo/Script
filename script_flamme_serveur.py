a54660a5466806290f68f468b29c0e0f1fe0f008
richardvo <richardvo@gmail.com>
2022-05-06 20:27:15 +0200

    correction

--- script_flamme_serveur.py
+++ script_flamme_serveur.py
@@ -6,38 +6,37 @@ import datetime
 import paho.mqtt.client as mqtt
 
 SERVEUR = '192.168.1.61'
+pin = 26
 
 def alerte_flamme(channel):
 
-    now = datetime.datetime.now()
-    client = mqtt.Client()
-    client.connect(SERVEUR, 1883, 60)
-    client.loop_start()
-    insertline = '{"alerte": {"type": "incendie", "info": {"type": "Flamme detectée", "emplacement": "garage", "equipement": "Serveur", "timestamp": "\'%s\'", "ack_alerte": false } }}'
-    new_line = insertline % now
-    topic = ("capteur/incendie/alerte")
-    client.publish(topic, new_line, 1)
-    client.loop_stop()
-    client.disconnect()
-
-
-def ack_alerte_flamme(channel):
-
-    now = datetime.datetime.now()
-    client = mqtt.Client()
-    client.connect(SERVEUR, 1883, 60)
-    client.loop_start()
-    insertline = '{"alerte": {"type": "incendie", "info": {"type": "Acquittement flamme detectée", "emplacement": "garage", "equipement": "Serveur", "timestamp": "\'%s\'", "ack_alerte": true } }}'
-    new_line = insertline % now
-    topic = ("capteur/incendie/alerte")
-    client.publish(topic, new_line, 1)
-    client.loop_stop()
-    client.disconnect()
+    if GPIO.input(pin):
+        now = datetime.datetime.now()
+        client = mqtt.Client()
+        client.connect(SERVEUR, 1883, 60)
+        client.loop_start()
+        insertline = '{"alerte": {"type": "incendie", "info": {"type": "Flamme detectée", "emplacement": "garage", "equipement": "Serveur", "timestamp": "\'%s\'", "ack_alerte": false } }}'
+        new_line = insertline % now
+        topic = ("capteur/incendie/alerte")
+        client.publish(topic, new_line, 1)
+        client.loop_stop()
+        client.disconnect()
+    
+    else:
+        now = datetime.datetime.now()
+        client = mqtt.Client()
+        client.connect(SERVEUR, 1883, 60)
+        client.loop_start()
+        insertline = '{"alerte": {"type": "incendie", "info": {"type": "Acquittement flamme detectée", "emplacement": "garage", "equipement": "Serveur", "timestamp": "\'%s\'", "ack_alerte": true } }}'
+        new_line = insertline % now
+        topic = ("capteur/incendie/alerte")
+        client.publish(topic, new_line, 1)
+        client.loop_stop()
+        client.disconnect()
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DN)
-GPIO.add_event_detect(18, GPIO.RISING, callback=alerte_flamme, bouncetime=500)
-GPIO.add_event_detect(18, GPIO.FALLING, callback=ack_alerte_flamme, bouncetime=500)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(pin, GPIO.BOTH, callback=alerte_flamme, bouncetime=500)
 
 
 while True:
